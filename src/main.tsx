ab8eb3a65532880e1f2772b5fb4c815d021d8415
Quirax Lee <quiraxical@gmail.com>
2025-11-16 09:25:57 +0900

    기본 react-router 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { MainPage } from "./pages/main/index.tsx";
+import { DefaultLayout } from "./layout/default.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    Component: DefaultLayout,
+    children: [
+      {
+        index: true,
+        Component: MainPage,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
