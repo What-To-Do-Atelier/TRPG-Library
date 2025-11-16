9653a2066d7e0d5a720c5c606f53b78a3b75f68b
Quirax Lee <quiraxical@gmail.com>
2025-11-16 09:32:05 +0900

    Tailwind CSS 추가 및 설정

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router";
 export function DefaultLayout() {
   return (
     <>
+      <header className="h-16 bg-red-500">Header</header>
       <Outlet />
     </>
   );
