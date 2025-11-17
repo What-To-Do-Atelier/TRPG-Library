097456baabfe40a2b0092e8f4f1af3d858415ee1
Quirax Lee <quiraxical@gmail.com>
2025-11-17 20:43:19 +0900

    헤더 내용의 기초적인 배치

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -3,9 +3,14 @@ import { Outlet } from "react-router";
 export function DefaultLayout() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-      <header className="flex justify-between p-2 border-b h-16 flex-none fixed w-full bg-white">
-        <div className="">로고 및 타이틀</div>
-        <div className="">로그인 버튼</div>
+      <header className="flex justify-between p-2 border-b h-16 flex-none fixed w-full bg-white items-center">
+        <div className="flex gap-1">
+          <div className="">로고</div>
+          <div className="">타이틀</div>
+        </div>
+        <div className="">
+          <button className="">로그인</button>
+        </div>
       </header>
       <main className="flex-1 mt-16">
         <Outlet />
