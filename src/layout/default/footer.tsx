b93839f815ef3aafb266ddf9832bc69847825a68
Quirax Lee <quiraxical@gmail.com>
2025-11-19 21:21:11 +0900

    사이트 링크 버튼 아이콘으로 변환

--- src/layout/default/footer.tsx
+++ src/layout/default/footer.tsx
@@ -1,5 +1,11 @@
-import { SiX } from "@icons-pack/react-simple-icons";
-import { type PropsWithChildren } from "react";
+import {
+    SiBluesky,
+    SiMastodon,
+    SiTistory,
+    SiX,
+    type IconType,
+} from "@icons-pack/react-simple-icons";
+import { type PropsWithChildren, type ReactElement } from "react";
 
 const TextButton = ({ children }: PropsWithChildren) => (
     <button className="cursor-pointer hover:underline hover:underline-offset-6">
@@ -7,13 +13,19 @@ const TextButton = ({ children }: PropsWithChildren) => (
     </button>
 );
 
-const IconButton = ({ children }: PropsWithChildren) => (
-    <button className="hover:bg-black/15 p-2 transition rounded-xl">
-        {children}
-    </button>
-);
+const IconButton = ({ children }: { children: ReactElement<IconType> }) => {
+    return (
+        <button className="hover:bg-black/15 p-2 transition rounded-xl">
+            {children}
+        </button>
+    );
+};
 
 export default function Footer() {
+    const iconProps = {
+        size: 20,
+    };
+
     return (
         <footer className="flex-none border-t p-10 flex justify-center gap-6 items-center">
             <div className="flex gap-2">
@@ -23,12 +35,20 @@ export default function Footer() {
             <TextButton>공지사항</TextButton>
             <TextButton>이용약관</TextButton>
             <TextButton>개인정보 처리방침</TextButton>
-            <IconButton>
-                <SiX />
-            </IconButton>
-            <button className="">Mastodon</button>
-            <button className="">Bluesky</button>
-            <button className="">Tistory</button>
+            <div className="flex gap-2">
+                <IconButton>
+                    <SiX {...iconProps} />
+                </IconButton>
+                <IconButton>
+                    <SiMastodon {...iconProps} />
+                </IconButton>
+                <IconButton>
+                    <SiBluesky {...iconProps} />
+                </IconButton>
+                <IconButton>
+                    <SiTistory {...iconProps} />
+                </IconButton>
+            </div>
         </footer>
     );
 }
