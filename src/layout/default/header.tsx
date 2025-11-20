37a4d42f79fe6232e6cdf9b3bd3f2900d4857b03
Quirax Lee <quiraxical@gmail.com>
2025-11-20 21:48:13 +0900

    헤더 고정 스타일 수정 및 메인 레이아웃 간격 조정

--- src/layout/default/header.tsx
+++ src/layout/default/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="flex justify-between px-6 border-b h-16 flex-none fixed w-full bg-white items-center">
+        <header className="flex justify-between px-6 border-b h-16 flex-none w-full bg-white items-center">
             <div className="flex gap-1">
                 <div className="">로고</div>
                 <div className="">TRPG Library</div>
--- src/layout/default/index.tsx
+++ src/layout/default/index.tsx
@@ -6,7 +6,7 @@ export function DefaultLayout() {
     return (
         <div className="flex flex-col min-h-screen overflow-hidden">
             <Header />
-            <main className="flex-1 mt-16">
+            <main className="flex-1">
                 <Outlet />
             </main>
             <Footer />
